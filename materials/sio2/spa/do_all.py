d26851f0384dd0d38f5b5d6ab86d86b0ff10c64e
olgaridzel <olgaridzel@yandex.ru>
2023-03-08 08:49:43 -0500

    sio2 elf

--- materials/sio2/spa/do_all.py
+++ materials/sio2/spa/do_all.py
@@ -5,7 +5,7 @@ energies = [20, 30, 40, 50, 100, 200, 300, 500, 700, 980]
 
 def run_mast(i):
 	print(energies[i])
-	os.system("mast_sey -e {} -m 1000 -ins -dos -vale 16 > out/{}.stdout.txt".format(energies[i],energies[i]))
+	os.system("mast_sey -e {} -m 1000 -ins -dos -vale 16 -nostep > out/{}.stdout.txt".format(energies[i],energies[i]))
 
 
 if __name__ == '__main__':
